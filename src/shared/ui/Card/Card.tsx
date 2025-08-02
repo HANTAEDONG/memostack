c794d75f53751e3595a9af53763d2e2bcee94819
HANTAEDONG <htd0913@gmail.com>
2025-08-02 15:46:03 +0900

    add: Card 컴포넌트를 새로 추가하여 UI 구성 요소를 개선함

---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/shared/lib/utils";
+
+type CardProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Card = ({ children, className }: CardProps) => {
+  return (
+    <div
+      className={cn(
+        "bg-[rgb(243,244,246)] border-gray-200 dark:bg-white rounded-lg border dark:border-gray-200 p-2 w-fit",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
