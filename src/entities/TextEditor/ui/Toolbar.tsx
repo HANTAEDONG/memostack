02c9352dff0a54b8defa12b360f599860b487474
HANTAEDONG <htd0913@gmail.com>
2025-08-03 14:53:19 +0900

    update: 툴바 컴포넌트에 상태 관리 및 링크 추가 기능을 통합하고, UI 구성 요소를 개선함

--- src/entities/TextEditor/ui/Toolbar.tsx
+++ src/entities/TextEditor/ui/Toolbar.tsx
@@ -2,115 +2,241 @@ import { Editor } from "@tiptap/react";
 import ToolbarButton from "./ToolbarButton";
 import ToolbarIcon from "./ToolbarIcon";
 import ToolbarDropDown from "./ToolbarDropDown";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useEditorState } from "../model/useEditorState";
-import { getEditorActions } from "../model/editor.actions";
+
 import {
-  getActiveOptions,
   headingOptions,
   listOptions,
+  toolbarOptions,
 } from "../model/editor.options";
+import Card from "@/shared/ui/Card/Card";
 
-const Toolbar = ({ editor }: { editor: Editor }) => {
-  const actions = getEditorActions(editor);
+interface ToolbarProps {
+  editor: Editor;
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const Toolbar = ({ editor, isDarkMode, toggleDarkMode }: ToolbarProps) => {
   const ref = useRef<HTMLDivElement>(null);
   const { editorState } = useEditorState(editor);
-
-  const activeOptions = getActiveOptions(editor);
-  console.log(
-    "현재 활성화된 옵션들:",
-    activeOptions.map((opt) => opt.label)
-  );
-
+  const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState("");
+  const [mention, setMention] = useState("");
   return (
-    <div className="w-full h-[44px] relative flex justify-center items-center bg-white dark:bg-[rgb(14, 14, 17)] border-b border-gray-200">
+    <div className="w-full h-[44px] relative border-b border-gray-200 rounded-t-lg">
       <div
-        className="flex gap-2 dark:text-white"
-        style={{ color: "rgb(14, 14, 17)" }}
+        className="w-full h-full overflow-x-auto"
+        style={{
+          scrollbarWidth: "none",
+          msOverflowStyle: "none",
+        }}
       >
-        <div className="flex gap-1 border-r border-gray-200 pr-2">
-          <ToolbarButton
-            className={`w-8 h-8 flex items-center justify-center ${
-              editor?.can().undo() ? "text-white" : "text-gray-500"
-            }`}
-            onClick={() => {
-              actions?.undo();
-            }}
-            disabled={!editor?.can().undo()}
+        <div
+          className="flex gap-2 dark:text-white min-w-max px-4 whitespace-nowrap h-full items-center"
+          style={{ color: "rgb(14, 14, 17)" }}
+        >
+          <div className="flex gap-1 border-r border-gray-200 pr-2">
+            <ToolbarButton
+              className={`w-8 h-8 flex items-center justify-center`}
+              onClick={() => {
+                toolbarOptions.undo.action(editor);
+              }}
+              disabled={!editor?.can().undo()}
+            >
+              <ToolbarIcon
+                name="Undo"
+                className={`${
+                  editor?.can().undo()
+                    ? "text-gray-700 dark:text-[oklch(90%_0_0)]"
+                    : "text-gray-300 dark:text-gray-500"
+                }`}
+              />
+            </ToolbarButton>
+            <ToolbarButton
+              className={`w-8 h-8 flex items-center justify-center`}
+              onClick={() => {
+                toolbarOptions.redo.action(editor);
+              }}
+              disabled={!editor?.can().redo()}
+            >
+              <ToolbarIcon
+                name="Redo"
+                className={`${
+                  editor?.can().redo()
+                    ? "text-gray-700 dark:text-[oklch(90%_0_0)]"
+                    : "text-gray-300 dark:text-gray-500"
+                }`}
+              />
+            </ToolbarButton>
+          </div>
+          <div className="flex gap-1 border-r border-gray-200 pr-2">
+            <ToolbarDropDown
+              options={headingOptions}
+              editor={editor}
+              ref={ref}
+            />
+            <ToolbarDropDown options={listOptions} editor={editor} ref={ref} />
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.codeBlock.action(editor);
+              }}
+            >
+              <ToolbarIcon name="Code" />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.blockquote.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="Quote"
+                isActive={editorState.activeNodes.blockquote}
+              />
+            </ToolbarButton>
+          </div>
+          <div className="flex gap-1 border-r border-gray-200 pr-2">
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.bold.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="Bold"
+                isActive={editorState.activeMarks.bold}
+              />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.italic.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="Italic"
+                isActive={editorState.activeMarks.italic}
+              />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.underline.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="Underline"
+                isActive={editorState.activeMarks.underline}
+              />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.strike.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="Strikethrough"
+                isActive={editorState.activeMarks.strike}
+              />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.highlight.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="Highlighter"
+                isActive={editorState.activeMarks.highlight}
+              />
+            </ToolbarButton>
+            {/* <ToolbarButton
+            onClick={useCallback(() => actions?.setLink(), [actions])}
           >
-            <ToolbarIcon name="Undo" />
-          </ToolbarButton>
-          <ToolbarButton
-            className={`w-8 h-8 flex items-center justify-center ${
-              editor?.can().redo() ? "text-white" : "text-gray-500"
-            }`}
-            onClick={() => actions?.redo()}
-            disabled={!editor?.can().redo()}
-          >
-            <ToolbarIcon name="Redo" />
-          </ToolbarButton>
-        </div>
-        <div className="flex gap-1 border-r border-gray-200 pr-2">
-          <ToolbarDropDown options={headingOptions} editor={editor} ref={ref} />
-          <ToolbarDropDown options={listOptions} editor={editor} ref={ref} />
-          <ToolbarButton onClick={() => actions?.toggleCodeBlock()}>
-            <ToolbarIcon name="Code" />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.toggleBlockquote()}>
-            <ToolbarIcon
-              name="Quote"
-              isActive={editorState.activeNodes.blockquote}
-            />
-          </ToolbarButton>
-        </div>
-        <div className="flex gap-1 border-r border-gray-200 pr-2">
-          <ToolbarButton onClick={() => actions?.toggleBold()}>
-            <ToolbarIcon name="Bold" isActive={editorState.activeMarks.bold} />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.toggleItalic()}>
-            <ToolbarIcon
-              name="Italic"
-              isActive={editorState.activeMarks.italic}
-            />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.toggleUnderline()}>
-            <ToolbarIcon
-              name="Underline"
-              isActive={editorState.activeMarks.underline}
-            />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.toggleStrike()}>
-            <ToolbarIcon
-              name="Strikethrough"
-              isActive={editorState.activeMarks.strike}
-            />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.toggleHighlight()}>
-            <ToolbarIcon
-              name="Highlighter"
-              isActive={editorState.activeMarks.highlight}
-            />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.toggleLink()}>
             <ToolbarIcon name="Link" isActive={editorState.activeMarks.link} />
-          </ToolbarButton>
-        </div>
-        <div className="flex gap-1 border-r border-gray-200 pr-2">
-          <ToolbarButton onClick={() => actions?.setTextAlign("left")}>
-            <ToolbarIcon
-              name="AlignLeft"
-              isActive={editorState.activeNodes.textAlign === "left"}
-            />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.setTextAlign("center")}>
-            <ToolbarIcon name="AlignCenter" />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.setTextAlign("right")}>
-            <ToolbarIcon name="AlignRight" />
-          </ToolbarButton>
-          <ToolbarButton onClick={() => actions?.setTextAlign("justify")}>
-            <ToolbarIcon name="AlignJustify" />
-          </ToolbarButton>
+          </ToolbarButton> */}
+            <ToolbarButton onClick={() => setOpen(true)}>
+              <ToolbarIcon
+                name="Link"
+                isActive={editorState.activeMarks.link}
+              />
+              {open && (
+                <Card className="absolute top-10 left-0 w-95 z-10 px-6 py-3">
+                  <div className="flex flex-col gap-2 w-fit">
+                    <div className="flex flex-col gap-1">
+                      <div className="text-sm font-semibold text-start">
+                        URL
+                      </div>
+                      <input
+                        className="w-83 h-full outline-none border border-gray-300 focus:border-gray-400 dark:border-gray-500 px-2 py-2 rounded-md bg-gray-200 opacity-80 dark:bg-gray-800 text-md"
+                        placeholder="example.com"
+                        value={url}
+                        onChange={(e) => {
+                          setUrl(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                      <div className="text-sm font-semibold text-start">
+                        Mention
+                      </div>
+                      <input
+                        className="w-full h-full outline-none border border-gray-300 focus:border-gray-400 dark:border-gray-500 px-2 py-2 rounded-md bg-gray-200 opacity-80 dark:bg-gray-800 text-md"
+                        placeholder="example"
+                        value={mention}
+                        onChange={(e) => {
+                          setMention(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="flex gap-2 justify-center">
+                      <button className="w-full h-full outline-none border border-gray-300 focus:border-gray-400 dark:border-gray-500 px-2 py-2 rounded-md bg-gray-200 opacity-80 dark:bg-gray-800 text-md">
+                        Cancel
+                      </button>
+                      <button className="w-full h-full outline-none border border-gray-300 focus:border-gray-400 dark:border-gray-500 px-2 py-2 rounded-md bg-blue-500 text-white text-semibold">
+                        Apply
+                      </button>
+                    </div>
+                  </div>
+                </Card>
+              )}
+            </ToolbarButton>
+          </div>
+          <div className="flex gap-1 border-r border-gray-200 pr-2">
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.textAlignLeft.action(editor);
+              }}
+            >
+              <ToolbarIcon
+                name="AlignLeft"
+                isActive={editorState.activeNodes.textAlign === "left"}
+              />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.textAlignCenter.action(editor);
+              }}
+            >
+              <ToolbarIcon name="AlignCenter" />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.textAlignRight.action(editor);
+              }}
+            >
+              <ToolbarIcon name="AlignRight" />
+            </ToolbarButton>
+            <ToolbarButton
+              onClick={() => {
+                toolbarOptions.textAlignJustify.action(editor);
+              }}
+            >
+              <ToolbarIcon name="AlignJustify" />
+            </ToolbarButton>
+          </div>
+          <div>
+            <ToolbarButton onClick={toggleDarkMode}>
+              <ToolbarIcon name={isDarkMode ? "Sun" : "Moon"} />
+            </ToolbarButton>
+          </div>
         </div>
       </div>
     </div>
