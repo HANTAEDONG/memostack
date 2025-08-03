72029fc140aa438189fe47b51337a85e05651826
HANTAEDONG <htd0913@gmail.com>
2025-08-03 15:18:33 +0900

    update: Tiptap 컴포넌트에서 EditorOptions를 사용하여 초기화 방식을 개선하고, 다크 모드 토글 기능을 추가함

--- src/entities/TextEditor/ui/Tiptap.tsx
+++ src/entities/TextEditor/ui/Tiptap.tsx
@@ -1,39 +1,35 @@
 "use client";
 
-import StarterKit from "@tiptap/starter-kit";
-import Highlight from "@tiptap/extension-highlight";
 import Toolbar from "./Toolbar";
-import TextAlign from "@tiptap/extension-text-align";
 import { EditorContent, useEditor } from "@tiptap/react";
+import EditorOptions from "../model/EditorOptions";
+import { useEffect, useState } from "react";
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Highlight,
-      TextAlign.configure({
-        types: ["heading", "paragraph"],
-      }),
-    ],
-    immediatelyRender: false,
-    autofocus: false,
-    editorProps: {
-      attributes: {
-        class:
-          "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
-      },
-    },
-    onUpdate: ({ editor }) => {
-      editor.view.updateState(editor.view.state);
-    },
-    onSelectionUpdate: ({ editor }) => {
-      editor.view.updateState(editor.view.state);
-    },
-  });
-
+  const editor = useEditor(EditorOptions);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  useEffect(() => {
+    if (isDarkMode) {
+      document.getElementById("tiptap-editor")?.classList.add("dark");
+    } else {
+      document.getElementById("tiptap-editor")?.classList.remove("dark");
+    }
+  }, [isDarkMode]);
   return (
-    <div className="bg-blue-300 dark:bg-[rgb(14, 14, 17)] rounded-lg flex flex-col px-2">
-      {editor && <Toolbar editor={editor} />}
+    <div
+      className="relative border border-gray-200 rounded-lg flex flex-col bg-white dark:bg-[oklch(14.5%_0_0)] w-[723px] max-md:w-full"
+      id="tiptap-editor"
+    >
+      {editor && (
+        <Toolbar
+          editor={editor}
+          isDarkMode={isDarkMode}
+          toggleDarkMode={toggleDarkMode}
+        />
+      )}
       <EditorContent editor={editor} />
     </div>
   );
