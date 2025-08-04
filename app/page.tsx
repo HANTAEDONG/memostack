c58534593fd66a2c01479a0731f9e775318d11ab
HANTAEDONG <htd0913@gmail.com>
2025-08-04 14:27:42 +0900

    move: 기본 레이아웃, 홈 페이지 및 인증 API를 새로 추가하여 애플리케이션 구조를 설정함

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Tiptap from "@/entities/TextEditor/ui/Tiptap";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <Tiptap />
+    </div>
+  );
+}
