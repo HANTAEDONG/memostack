fb2c25ad2cf248580779470823f358937579fa93
HANTAEDONG <htd0913@gmail.com>
2025-08-11 12:14:11 +0900

    refactor: Provider 컴포넌트를 수정하여 에러 핸들링, 세션 관리 및 Apollo Client를 통합하고, Props 인터페이스 이름을 변경함.

--- src/shared/layout/Provider.tsx
+++ src/shared/layout/Provider.tsx
@@ -1,24 +1,39 @@
-import { SidebarProvider, SidebarTrigger } from "@/shared/ui/sidebar";
-import { AppSidebar } from "@/widgets/AppSidebar";
+"use client";
 
-interface ProviderProps {
+import { SessionProvider } from "next-auth/react";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "@/shared/lib/apollo-client";
+import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
+import { AppSidebar } from "@/widgets/AppSidebar";
+import { ErrorProvider } from "./ErrorProvider";
+import { ErrorBoundary } from "../ui/ErrorBoundary";
+
+interface ProvidersProps {
   children: React.ReactNode;
 }
 
-export function Provider({ children }: ProviderProps) {
+export default function Providers({ children }: ProvidersProps) {
   return (
-    <SidebarProvider defaultOpen={true}>
-      <AppSidebar />
-      <main
-        className={
-          "bg-background dark:bg-foreground w-[723px] mx-auto relative"
-        }
-      >
-        <div className="absolute top-2 left-2 z-10">
-          <SidebarTrigger className="cursor-pointer" />
-        </div>
-        {children}
-      </main>
-    </SidebarProvider>
+    <ErrorBoundary>
+      <SessionProvider>
+        <ApolloProvider client={apolloClient}>
+          <ErrorProvider>
+            <SidebarProvider defaultOpen={true}>
+              <AppSidebar />
+              <main
+                className={
+                  "bg-background dark:bg-foreground w-[723px] mx-auto relative"
+                }
+              >
+                <div className="absolute top-2 left-2 z-10">
+                  <SidebarTrigger className="cursor-pointer" />
+                </div>
+                {children}
+              </main>
+            </SidebarProvider>
+          </ErrorProvider>
+        </ApolloProvider>
+      </SessionProvider>
+    </ErrorBoundary>
   );
 }
